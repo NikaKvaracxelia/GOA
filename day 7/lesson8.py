db395019b0365a80f1c631ae9c31c1e5a925c184
NikaKvaracxelia <Nika85802@gmail.com>
2024-02-24 19:53:04 +0400

    third commit

---
+++ day 7/lesson8.py
@@ -0,0 +1,18 @@
+num1 = float(input("Please enter your num1"))
+num2 = float(input("Please enter your num2"))
+
+print(num1+num2)
+
+num1 = int(input("Please enter your num1"))
+num2 = int(input("Please enter your num2"))
+
+
+print(num1+num2)
+
+
+
+num1 = str(input("Please enter your num1"))
+num2 = str(input("Please enter your num2"))
+
+
+print(num1+num2)
