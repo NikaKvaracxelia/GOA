80ade171ac8849862de86948080876c35f34d42b
NikaKvaracxelia <Nika85802@gmail.com>
2024-03-07 19:08:36 +0400

    third commit

--- day 0/lessson1.py
+++ day 0/lessson1.py
@@ -23,12 +23,14 @@ left(90)
 #draw a door
 forward(70)
 color("yellow")
+begin_fill()
 left(90)
 forward(120)   #height of door
 right(90)
 forward(60)
 right(90)
 forward(120)
+end_fill()
 
 #end of door
 
---
+++ day 10/lesson11.py
@@ -0,0 +1,22 @@
+for i in range(0,21):
+    print(i)
+
+for i in range (140,250):
+    print(i)
+
+for i in range(50,101):
+    print(i)
+
+for i in range(100,51):
+    print(i)
+
+for i in range(20,41):
+    print(i+i)
+
+for i in range(100,239):
+    print(i)
+
+num = int(input("Please enter your num"))
+
+for i in range(14,25):
+    print(i)
