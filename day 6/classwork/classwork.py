baadf58ad5821449a0fceb158677764f831510b8
NikaKvaracxelia <Nika85802@gmail.com>
2024-03-21 21:33:21 +0400

    third commit

--- day 6/classwork/classwork.py
+++ day 6/classwork/classwork.py
@@ -5,3 +5,5 @@ num3 = int(input("pelase enter your num3"))
 print(num1+num2+num3)
 
 print("join goa ")
+print("join goa ")
+print("join goa ")
--- day 8/classwork/classwork.py
+++ day 8/classwork/classwork.py
@@ -1,2 +1,4 @@
 print("join goa ")
 print("join goa ")
+print("join goa ")
+print("join goa ")
